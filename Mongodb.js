4017eacd3e49f4605cead90d42002685cc4d4e68
Ameen-mohammed-Adam <ameen2bman@gmail.com>
2025-09-21 18:24:47 +0300

    added First Admin Creation

--- Mongodb.js
+++ Mongodb.js
@@ -1,12 +1,22 @@
 const mongoose = require("mongoose");
+const User = require("./modles/userSchema");
+require("dotenv").config();
 let isconnected;
 
 const connectDB = async (uri) => {
   if (isconnected) return;
   mongoose
     .connect(uri)
-    .then((conn) => {
+    .then(async (conn) => {
       isconnected = conn.connections[0].readyState;
+      const user = await User.findOne({ email: process.env.FIRST_ADMIN_EMAIL });
+      if (!user) {
+        await User.create({
+          email: process.env.FIRST_ADMIN_EMAIL,
+          password: process.env.FIRST_ADMIN_PASSWORD,
+          role: "admin",
+        });
+      }
       console.log("MongoDB Connected!");
     })
     .catch((err) => {
