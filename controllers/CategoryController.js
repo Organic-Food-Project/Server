bed779dd90b48bb68aba725c36243f3dc97df6ca
Ameen-mohammed-Adam <ameen2bman@gmail.com>
2025-09-23 23:57:13 +0300

    Added The Product And Category Constrollers

---
+++ controllers/CategoryController.js
@@ -0,0 +1,60 @@
+const Categories = require("../modles/categariesSchema");
+const AppError = require("../utils/AppError");
+exports.getallCategories = async (req, res, next) => {
+  try {
+    const categories = await Categories.find();
+    res.status(200).json({ categories });
+  } catch (err) {
+    console.error(err);
+  }
+};
+exports.addCategory = async (req, res, next) => {
+  try {
+    const user = req.user || {};
+    if (!user || user.role !== "admin") {
+      throw new AppError("You Are Not Allowed To Do This.", 403);
+    }
+    const { name, count, image } = req.body || {};
+    if (!name || count || !image) {
+      throw new AppError(
+        "Please Provide (name & count = 0 & image) for the category",
+        400
+      );
+    }
+    const check = await Categories.findOne({ name });
+    if (check) {
+      throw new AppError("This Category Exists!", 400);
+    }
+    const cat = await Categories.create({
+      name,
+      count,
+      image,
+    });
+    res
+      .status(201)
+      .json({ status: "Success", message: "Added The New Category!" });
+  } catch (err) {
+    next(err);
+  }
+};
+exports.deleteCategory = async (req, res, next) => {
+  try {
+    const user = req.user || {};
+    if (!user || user.role !== "admin") {
+      throw new AppError("You Are Not Allowed To Do This.", 403);
+    }
+    const { name } = req.body || {};
+    if (!name) {
+      throw new AppError("Provide What You Want To Delete", 400);
+    }
+    const cat = await Categories.findOneAndDelete({ name });
+    if (!cat) {
+      throw new AppError("Category Not Found", 404);
+    }
+    res
+      .status(201)
+      .json({ status: "Success", message: "Category Deleted Successfuly" });
+  } catch (err) {
+    next(err);
+  }
+};
---
+++ controllers/ProductControllers.js
@@ -0,0 +1,95 @@
+const file = require("../modles/porductSchema");
+const Products = file.Products;
+const Catogaries = require("../modles/categariesSchema");
+const AppError = require("../utils/AppError");
+exports.getAllProducts = async (req, res, next) => {
+  try {
+    const products = await Products.find();
+    res.status(200).json({ products });
+  } catch (err) {
+    next(err);
+  }
+};
+exports.getProductByName = async (req, res, next) => {
+  try {
+    const { name } = req.body;
+    const product = await Products.findOne({ name });
+    if (!product) {
+      res.status(404).json({ message: "Product Not Found" });
+    }
+    res.status(200).json({ product });
+  } catch (err) {
+    next(err);
+  }
+};
+exports.addNewProduct = async (req, res, next) => {
+  try {
+    const user = req.user || {};
+    if (!user || user.role !== "admin") {
+      throw new AppError("You Are Not Allowed To Do This.", 403);
+    }
+    const { description, category, quantity, price, images, name } =
+      req.body || {};
+    if (!description || !category || !quantity || !price || !images || !name) {
+      throw new AppError("Make Sure You Added All The Data.", 400);
+    }
+    const checkName = await Products.findOne({ name });
+    if (checkName) {
+      throw new AppError("This Product Already Exists", 400);
+    }
+    const cat = await Catogaries.findOne({ name: category });
+    if (!cat) {
+      throw new AppError("Catogery Does Not Exist.", 404);
+    }
+    const product = await Products.create({
+      description,
+      category,
+      quantity,
+      price,
+      images,
+      name,
+    });
+    cat.products.push(product._id);
+    cat.count += product.quantity;
+    await cat.save();
+    res
+      .status(201)
+      .json({ status: "Success", message: "Product Created.", product });
+  } catch (err) {
+    next(err);
+  }
+};
+exports.updateProduct = async (req, res, next) => {
+  try {
+    const user = req.user;
+    if (!user || user.role !== "admin") {
+      throw new AppError("You Are Not Allowed To Do This.", 403);
+    }
+    const { name } = req.body;
+    const product = Products.findOneAndUpdate({ name });
+    if (!product) {
+      throw new AppError("Product Not Found", 404);
+    }
+    res
+      .status(201)
+      .json({ status: "Success", message: "Product Edited Succesfuly" });
+  } catch (err) {
+    next(err);
+  }
+};
+exports.deleteProduct = async (req, res, next) => {
+  try {
+    const user = req.user;
+    if (!user || user.role !== "admin") {
+      throw new AppError("You Are Not Allowed To Do This.", 403);
+    }
+    const { name } = req.body;
+    const product = Products.findOneAndDelete({ name });
+    if (!product) {
+      throw new AppError("Product Not Found", 404);
+    }
+    res.status(200).json({ message: "Product Deleted Successfuly" });
+  } catch (err) {
+    next(err);
+  }
+};
