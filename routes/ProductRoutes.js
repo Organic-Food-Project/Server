a474c959c16645f55470e98c8863a591976f63bc
Ameen-mohammed-Adam <ameen2bman@gmail.com>
2025-09-29 20:10:32 +0300

    Refactor product routes for improved readability and organization

--- routes/ProductRoutes.js
+++ routes/ProductRoutes.js
@@ -6,15 +6,16 @@ const ImageValidate = require("../middlerwares/Image_Validate");
 const upload = require("../middlerwares/multer");
 const UploadImage = require("../middlerwares/Image_kit");
 //
-Router.get(
+Router.get("/", Products.getAllProducts);
+Router.get("/:name", Products.getProductByName);
+Router.post(
   "/",
+  auth.protect,
   upload.array("ProductImages", 4),
   ImageValidate(true),
   UploadImage(true),
-  Products.getAllProducts
+  Products.addNewProduct
 );
-Router.get("/:name", Products.getProductByName);
-Router.post("/", auth.protect, Products.addNewProduct);
 Router.delete("/:name", auth.protect, Products.deleteProduct);
 
 module.exports = Router;
