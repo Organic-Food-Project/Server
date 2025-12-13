d5ec29e812f921ad83c7ca5e8b6307efe9e2ed58
Ameen-mohammed-Adam <ameen2bman@gmail.com>
2025-12-13 15:40:23 +0200

    Implement pagination for order history and update payment schema to use 'total' instead of 'price'

--- controllers/UserControlers.js
+++ controllers/UserControlers.js
@@ -139,10 +139,35 @@ exports.orderHistory = async (req, res, next) => {
     if (!user) {
       throw new AppError("sign in again");
     }
-    const orders = await Payment.find({
-      _id: { $in: req.user.purchase_history },
+    const limit = req.query.limit * 1 || 1;
+    const page = req.query.page * 1 || 1;
+    const skip = (page - 1) * limit;
+    let orders = Payment.find(
+      {
+        _id: { $in: req.user.purchase_history },
+      },
+      "_id createdAt total"
+    );
+    orders.skip(skip).limit(limit);
+    if (req.query.page) {
+      if (skip >= req.user.purchase_history.length) {
+        res.status(200).json({
+          data: [],
+          meta: {
+            limit,
+            total: req.user.purchase_history.length,
+          },
+        });
+      }
+    }
+    orders = await orders.lean();
+    res.status(200).json({
+      data: orders,
+      meta: {
+        limit,
+        total: req.user.purchase_history.length,
+      },
     });
-    Response(res, 200, orders);
   } catch (error) {
     next(error);
   }
--- modles/paymentSchema.js
+++ modles/paymentSchema.js
@@ -14,19 +14,13 @@ const PaymentSchema = mongoose.Schema(
       required: [true, "A Payment must hvae a user"],
       ref: "User",
     },
-    price: {
+    total: {
       type: Number,
       required: [true, "A Payment must hvae a price"],
     },
   },
   { timestamps: true }
 );
-PaymentSchema.pre(/^find/, function () {
-  this.populate({
-    path: "user",
-    select: "_id firstName lastName email",
-  }).populate({ path: "products", select: "_id name price rate description" });
-});
 const Payment = mongoose.model("payment", PaymentSchema);
 
 module.exports = Payment;
